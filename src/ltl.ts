1a458ccae0504ad5c1450571118d880eb36652bb
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2018-10-26 09:10:05 -0700

    Add pegjs grammar example

---
+++ src/ltl.ts
@@ -0,0 +1,24 @@
+var peg = require("pegjs");
+
+var parser = peg.generate(`
+start
+  = additive
+
+additive
+  = left:multiplicative "+" right:additive { return left + right; }
+  / multiplicative
+
+multiplicative
+  = left:primary "*" right:multiplicative { return left * right; }
+  / primary
+
+primary
+  = integer
+  / "(" additive:additive ")" { return additive; }
+
+integer "integer"
+  = digits:[0-9]+ { return parseInt(digits.join(""), 10); }
+`);
+
+console.log(parser.parse("1+1"));
+console.log(parser.buildMessage());
