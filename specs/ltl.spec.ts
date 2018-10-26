4543bd17419c7b85105a070b412c31e398684806
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2018-10-26 15:52:21 -0700

    Implement 'or' and its test

--- specs/ltl.spec.ts
+++ specs/ltl.spec.ts
@@ -1,30 +1,25 @@
 import * as fc from 'fast-check';
 import {evalT, LTLOperator} from '../src/ltl';
 
-const p1 = {
-  type: 'prop',
-  value: true,  // boolean
-};
-
-const p2 = {
-  type: 'prop',
-  value: false,  // boolean
-};
-
-const p3 = {
-  type: 'prop',
-  value: () => true,  // function (propositional logic)
-};
-
-// console.log(evalT(p1, ()=>{}));
-
-test('array', () => {
+test('and', () => {
   fc.assert(
     fc.property(fc.array(fc.boolean(), 2, 2), data => {
       expect(evalT({
         type: LTLOperator.and,
         value: data as any,
       })).toEqual(data.indexOf(false) === -1);
+    }),
+    {verbose: true},
+  );
+});
+
+test('or', () => {
+  fc.assert(
+    fc.property(fc.array(fc.boolean(), 2, 2), data => {
+      expect(evalT({
+        type: LTLOperator.or,
+        value: data as any,
+      })).toEqual(data.indexOf(true) !== -1);
     }),
     {verbose: true},
   );
--- src/ltl.ts
+++ src/ltl.ts
@@ -15,26 +15,37 @@ export function evalT(formula: LTLFormula, lookup?: (key: string) => boolean) {
     return formula;
   }
 
-  switch (formula.type) {
-    case LTLOperator.var:
-      return lookup(formula.value as string);
-    case LTLOperator.not:
-      const f = evalT(formula, lookup);
-      return typeof formula.value === 'boolean'
-        ? !formula.value : f;
-    case LTLOperator.and:
-      const fs = formula.value
-        .map(f => evalT(f, lookup))
-      if (fs.indexOf(false) !== -1)  // conjunction contains a false
-        return false;
-      const fsWOBool = fs.filter(f => typeof f !== 'boolean');
-      return fsWOBool.length === 0
-        ? true  // an empty conjunction is true
-        : {
-          type: LTLOperator.and,
-          value: fsWOBool,
-        };
-    default:
-      throw new Error(`Unknown type: ${formula.type}`);
+  if (formula.type === LTLOperator.var) {
+    return lookup(formula.value as string);
+  } else if (formula.type === LTLOperator.not) {
+    const f = evalT(formula, lookup);
+    return typeof formula.value === 'boolean'
+      ? !formula.value : f;
+  } else if (formula.type === LTLOperator.and) {
+    const fs = formula.value
+      .map(f => evalT(f, lookup));
+    if (fs.indexOf(false) !== -1)  // conjunction contains a false
+      return false;
+    const fsWOBool = fs.filter(f => typeof f !== 'boolean');
+    return fsWOBool.length === 0
+      ? true  // an empty conjunction is a conjunction of trues
+      : {
+        type: LTLOperator.and,
+        value: fsWOBool,
+      };
+  } else if (formula.type === LTLOperator.or) {
+    const fs = formula.value
+      .map(f => evalT(f, lookup));
+    if (fs.indexOf(true) !== -1)  // disjunction contains a true
+      return true;
+    const fsWOBool = fs.filter(f => typeof f !== 'boolean');
+    return fsWOBool.length === 0
+      ? false  // an empty disjunction is a disjunction of falses
+      : {
+        type: LTLOperator.or,
+        value: fsWOBool,
+      };
+  } else {
+    throw new Error(`Unknown type: ${formula.type}`);
   }
 }
