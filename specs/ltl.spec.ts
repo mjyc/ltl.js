47dd5fc2d21494556c8f434c0f447a2d7a944063
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2018-10-26 15:57:00 -0700

    Type formula

--- specs/ltl.spec.ts
+++ specs/ltl.spec.ts
@@ -1,12 +1,25 @@
 import * as fc from 'fast-check';
-import {evalT, LTLOperator} from '../src/ltl';
+import {evalT} from '../src/ltl';
+import {LTLOperator, LTLFormula} from '../src/ltl'
+
+// test('or', () => {
+//   fc.assert(
+//     fc.property(fc.boolean(), data => {
+//       expect(evalT({
+//         type: LTLOperator.or,
+//         value: data as any,
+//       })).toEqual(data.indexOf(true) !== -1);
+//     }),
+//     {verbose: true},
+//   );
+// });
 
 test('and', () => {
   fc.assert(
     fc.property(fc.array(fc.boolean(), 2, 2), data => {
       expect(evalT({
         type: LTLOperator.and,
-        value: data as any,
+        value: data as [LTLFormula],
       })).toEqual(data.indexOf(false) === -1);
     }),
     {verbose: true},
@@ -18,7 +31,7 @@ test('or', () => {
     fc.property(fc.array(fc.boolean(), 2, 2), data => {
       expect(evalT({
         type: LTLOperator.or,
-        value: data as any,
+        value: data as [LTLFormula],
       })).toEqual(data.indexOf(true) !== -1);
     }),
     {verbose: true},
--- src/ltl.ts
+++ src/ltl.ts
@@ -7,7 +7,7 @@ export enum LTLOperator {
 
 export type LTLFormula = {
   type: LTLOperator,
-  value: any// string | [LTLFormula],
+  value: string | [LTLFormula],
 } | boolean;
 
 export function evalT(formula: LTLFormula, lookup?: (key: string) => boolean) {
@@ -17,12 +17,14 @@ export function evalT(formula: LTLFormula, lookup?: (key: string) => boolean) {
 
   if (formula.type === LTLOperator.var) {
     return lookup(formula.value as string);
+
   } else if (formula.type === LTLOperator.not) {
     const f = evalT(formula, lookup);
     return typeof formula.value === 'boolean'
       ? !formula.value : f;
+
   } else if (formula.type === LTLOperator.and) {
-    const fs = formula.value
+    const fs = (formula.value as [LTLFormula])
       .map(f => evalT(f, lookup));
     if (fs.indexOf(false) !== -1)  // conjunction contains a false
       return false;
@@ -33,8 +35,9 @@ export function evalT(formula: LTLFormula, lookup?: (key: string) => boolean) {
         type: LTLOperator.and,
         value: fsWOBool,
       };
+
   } else if (formula.type === LTLOperator.or) {
-    const fs = formula.value
+    const fs = (formula.value as [LTLFormula])
       .map(f => evalT(f, lookup));
     if (fs.indexOf(true) !== -1)  // disjunction contains a true
       return true;
@@ -45,6 +48,7 @@ export function evalT(formula: LTLFormula, lookup?: (key: string) => boolean) {
         type: LTLOperator.or,
         value: fsWOBool,
       };
+
   } else {
     throw new Error(`Unknown type: ${formula.type}`);
   }
